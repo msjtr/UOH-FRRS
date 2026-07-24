9fb7b4173cabb08e7d6ffc8a2facbda6da89055d
msjtr <msjt301@gmail.com>
2026-07-24 20:44:24 +0300

    Update authConfig.js

--- app/src/authConfig.js
+++ app/src/authConfig.js
@@ -1,17 +1,23 @@
 export const msalConfig = {
   auth: {
     clientId: "bd9ad99a-c81a-44bf-aaf9-0a8f9584ab8a",
-    authority:
-      "https://login.microsoftonline.com/514faa19-8c84-4cef-8ae9-2b9dbce933cd",
+    authority: "https://login.microsoftonline.com/514faa19-8c84-4cef-8ae9-2b9dbce933cd",
     redirectUri: "https://uoh-frrs.onrender.com",
+    postLogoutRedirectUri: "https://uoh-frrs.onrender.com",
+    navigateToLoginRequestUrl: false,
   },
 
   cache: {
-    cacheLocation: "localStorage",
+    cacheLocation: "sessionStorage",
     storeAuthStateInCookie: false,
   },
 };
 
 export const loginRequest = {
-  scopes: ["User.Read"],
+  scopes: [
+    "openid",
+    "profile",
+    "email",
+    "User.Read",
+  ],
 };
