77ab98e13d11c5cb894d33e68ef061f2b6bccaf9
msjtr <msjt301@gmail.com>
2026-07-24 18:35:40 +0300

    Create authConfig.js

---
+++ app/src/authConfig.js
@@ -0,0 +1,19 @@
+export const msalConfig = {
+  auth: {
+    clientId: "bd9ed99a-c81a-44bf-aaf9-0a8f9584ab8a",
+    authority:
+      "https://login.microsoftonline.com/514faa19-8c84-4cef-8ae9-2b9dbce933cd",
+    redirectUri: "https://uoh-frrs.onrender.com",
+  },
+
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false,
+  },
+};
+
+export const loginRequest = {
+  scopes: [
+    "User.Read"
+  ]
+};
