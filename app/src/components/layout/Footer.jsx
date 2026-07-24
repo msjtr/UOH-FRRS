d239e29776f116d93a94c801565cdb7de63207a0
msjtr <msjt301@gmail.com>
2026-07-24 23:52:33 +0300

    Modify Footer component styling and content
    Updated footer height and added system description.

--- app/src/components/layout/Footer.jsx
+++ app/src/components/layout/Footer.jsx
@@ -2,16 +2,17 @@ export default function Footer() {
   return (
     <footer
       style={{
-        height: 50,
-        borderTop: "1px solid #e5e7eb",
+        height: 48,
         background: "#fff",
+        borderTop: "1px solid #e5e7eb",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         fontSize: 12,
+        color: "#666",
       }}
     >
-      © University of Ha'il
+      © University of Ha'il - Facilities Readiness Reporting System
     </footer>
   );
 }
