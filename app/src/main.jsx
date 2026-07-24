9e514eb8ebd6038296f36609cffdd32fb0265655
msjtr <msjt301@gmail.com>
2026-07-24 18:37:45 +0300

    Update main.jsx

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,12 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
-import App from "./App.jsx";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+
+import App from "./App";
+import { msalConfig } from "./authConfig";
+
+const msalInstance = new PublicClientApplication(msalConfig);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <FluentProvider theme={webLightTheme}>
-      <App />
-    </FluentProvider>
+    <MsalProvider instance={msalInstance}>
+      <FluentProvider theme={webLightTheme}>
+        <App />
+      </FluentProvider>
+    </MsalProvider>
   </StrictMode>
 );
