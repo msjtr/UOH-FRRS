a1cb5176773f0c37eb124d33d202e55ab197e0d9
msjtr <msjt301@gmail.com>
2026-07-24 20:51:53 +0300

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          msal: ["@azure/msal-browser", "@azure/msal-react"],
+          fluent: [
+            "@fluentui/react-components",
+            "@fluentui/react-icons",
+          ],
+          graph: ["@microsoft/microsoft-graph-client"],
+        },
+      },
+    },
+  },
+});
